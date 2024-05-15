8f2acd4fcd065d58456309ce58053805c6294892
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-15 16:01:51 +0000

    data conversion in javascript

---
+++ .devcontainer/01_basic/03_conversion datatype.js
@@ -0,0 +1,29 @@
+{let score = "33ad"
+ console.log(typeof score);}
+//Output=> "33abc" => NaN
+{
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);}
+// //"12 => 33"
+
+// // true =>1; false =>0;
+{let IsloggedIn = 1
+ let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);}
+
+
+{let num = "33ad"
+console.log(num);
+let valueInNumber = Number(num);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);}
+
+{let score = null
+console.log(typeof score);
+let valueInNumber =Number(score);
+console.log(typeof valueInNumber);}
+
+{let somenumber = 33
+let stringnumber = String(somenumber)
+console.log( typeof stringnumber)}
