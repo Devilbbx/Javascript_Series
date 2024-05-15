3618f9543400a1495237c1e543636146490d8565
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-15 15:06:47 +0000

    datatypes

--- .devcontainer/01_basic/02_datatypes.js
+++ .devcontainer/01_basic/02_datatypes.js
@@ -1,5 +1,25 @@
 "use strict";             // treat all js code as newer version
 //alert (3+3);              // we are using node js not browser
-console.log(3+3);
+console.log(3+3);   // code readabiltiy should be high
+console.log("Devendra")    // semicolon is optional in javascript
+
+// Datatype
+//number=> 2 to power 35
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value and also datatype
+// undefined => not assigned value
+// symbol => to find the uniqueness
+
+
+// object
+console.log(typeof"Devendra")
+//Output = string
+console.log(typeof null)
+//output = object
+console.log(typeof undefined)
+//output = undefined
+
 
 
