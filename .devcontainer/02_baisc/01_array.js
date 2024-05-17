35408596a44bdb5f12c9df5c370d22211d90d879
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-17 18:28:22 +0000

    Upload

--- .devcontainer/02_baisc/01_array.js
+++ .devcontainer/02_baisc/01_array.js
@@ -17,13 +17,43 @@ const myarr = [0,1,2,3,4,5]
  //console.log(myarr);
 
 //======================================= slice , splice ==========================================
-console.log("A",myarr);
-const mynewarr1 = myarr.slice(1,3)
+//console.log("A",myarr);
+//const mynewarr1 = myarr.slice(1,3)
 
-console.log(mynewarr1);
+//console.log(mynewarr1);
 
-const mynewarr2 = myarr.splice(1,3)
-console.log(mynewarr2);
+//const mynewarr2 = myarr.splice(1,3)
+//console.log(mynewarr2);
+
+
+const marvel_hero = ["thor","Iron Man","Starlord"];
+//console.log(marvel_hero);
+
+const dc_hero =["Superman","Flash"," BatMan"];
+//console.log(dc_hero);
+
+//console.log(marvel_hero);
+//console.log(marvel_hero[3][1]);
+
+const hero = marvel_hero.concat(dc_hero);
+//console.log(hero);                          // concat help to add two array in one array
+
+const all_new_hero = [...marvel_hero,...dc_hero]
+//console.log(all_new_hero);
+
+const another_array = [1,2,3,[4,5,6],7,[8,1,[10,9]]];
+const combined_array = another_array.flat(Infinity);   // cobime sub array within array and make it one aary
+console.log(combined_array);
+
+ console.log(Array.isArray("Devendra"));            // checking the array 
+ console.log(Array.from("Devendra"));              // to convert into array
+console.log(Array.from{{name: "Devendra"}})
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
 
 
 
