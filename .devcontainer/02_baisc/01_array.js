750212c03541723923d1ed6fc17c0f7275c6458f
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-17 17:48:48 +0000

    Array

--- .devcontainer/02_baisc/01_array.js
+++ .devcontainer/02_baisc/01_array.js
@@ -1,3 +1,27 @@
 // array
 const myarr = [0,1,2,3,4,5]
-console.log(myarr[0]);
+//console.log(myarr[0]);
+
+// Arrray methods
+//myarr.push(6) // Add new value to array
+//myarr.push(7)
+//myarr.pop()   // to remove the last value of the array
+//myarr.unshift(9) // to add the value in start
+//myarr.shift() 
+//console.log(myarr.includes(9));
+//console.log(myarr.indexOf(3));
+
+//const newarr = myarr.join() // have same data but chnage the datatype array into string
+ //console.log(newarr);
+ //console.log(typeof newarr);
+ //console.log(myarr);
+
+//======================================= slice , splice ==========================================
+console.log("A",myarr);
+let mynewarr1 = myarr.slice(1,3)
+
+console.log(mynewarr1);
+const mynewarr2 = myarr.splice(1,3)
+console.log(mynewarr2);
+
+
