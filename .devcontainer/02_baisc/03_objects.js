05345bf85312f1b3d4ff53928e826411c36237d7
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-18 10:40:58 +0000

    Upload

--- .devcontainer/02_baisc/03_objects.js
+++ .devcontainer/02_baisc/03_objects.js
@@ -22,9 +22,23 @@ const obj2 = {3: "c", 4:"d"}
 const obj3 = Object.assign({},obj1,obj2)
 //console.log(obj3);
 
-console.log(Object.keys(appuser));
-console.log(Object.values(appuser));
-console.log(Object.entries(appuser));
+//console.log(Object.keys(appuser));
+//console.log(Object.values(appuser));
+//console.log(Object.entries(appuser));
+//console.log(appuser.hasOwnProperty('IsLoggedIn'));
+
+
+const course = {
+    coursename : "Basic Javascript",
+    price : "999",
+    author : "Devendra Chapagain"
+}
+const {author} = course
+console.log(author);
+
+
+
+ 
 
 
 
