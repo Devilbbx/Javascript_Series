dcc0fd5130798ba0855c5d16a4b482879ea4cf8f
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-05-18 13:42:16 +0000

    Arrow javascript

---
+++ .devcontainer/03_basic/03_arrow.js
@@ -0,0 +1,17 @@
+
+let username ="23432864"
+const user = {
+    username : "Devendra",
+    price : 999,
+welcomeMessage : function(){
+    console.log(`${username}, welcome to website`);
+ console.log(this);
+}
+}
+//user.welcomeMessage();
+
+
+
+//user.username = "sam"
+user.welcomeMessage();
+// console.log(this);
